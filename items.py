245fe9ce3748d06573556ca76a3d7c9403346a7c
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2025-01-08 17:04:22 +0200

    Add edit item feature

--- items.py
+++ items.py
@@ -10,11 +10,19 @@ def get_items():
     return db.query(sql)
 
 def get_item(item_id):
-    sql = """SELECT items.title,
+    sql = """SELECT items.id,
+                    items.title,
                     items.description,
                     items.start_price,
+                    users.id user_id,
                     users.username
              FROM items, users
              WHERE items.user_id = users.id AND
                    items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, description):
+    sql = """UPDATE items SET title = ?,
+                              description = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, description, item_id])
