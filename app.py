91ea93694c97f6e6bd0523c93f40682827ce4c86
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2025-01-08 20:18:59 +0200

    Also check form in edit page

--- app.py
+++ app.py
@@ -81,7 +81,11 @@ def update_item():
         abort(403)
 
     title = request.form["title"]
+    if not title or len(title) > 50:
+        abort(403)
     description = request.form["description"]
+    if not description or len(description) > 1000:
+        abort(403)
 
     items.update_item(item_id, title, description)
 
