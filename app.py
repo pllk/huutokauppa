02eb9020514fa6753b5f405d26807e49ebd63018
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2025-01-10 16:21:47 +0200

    Add classes to items

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def show_item(item_id):
     item = items.get_item(item_id)
     if not item:
         abort(404)
-    return render_template("show_item.html", item=item)
+    classes = items.get_classes(item_id)
+    return render_template("show_item.html", item=item, classes=classes)
 
 @app.route("/new_item")
 def new_item():
@@ -64,7 +65,15 @@ def create_item():
         abort(403)
     user_id = session["user_id"]
 
-    items.add_item(title, description, start_price, user_id)
+    classes = []
+    section = request.form["section"]
+    if section:
+        classes.append(("Osasto", section))
+    condition = request.form["condition"]
+    if condition:
+        classes.append(("Kunto", condition))
+
+    items.add_item(title, description, start_price, user_id, classes)
 
     return redirect("/")
 
